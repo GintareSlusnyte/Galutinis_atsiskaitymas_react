193c3d6d63397d0ebdb8e627f62609c64754e269
GintareSlusnyte <gintareslusnyte@gmail.com>
2023-05-17 11:01:36 +0300

    pakoreguotas kelias index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import UsersContext from './contexts/UsersContext';
-import PostsContext from './contexts/PostsContext';
-import CommentsContext from './contexts/CommentsContext';
+import {UsersProvider} from './contexts/UsersContext';
+import {PostsProvider} from './contexts/PostsContext';
+import {CommentsProvider} from './contexts/CommentsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <UsersContext>
-            <PostsContext>
-                <CommentsContext>
+        <UsersProvider>
+            <PostsProvider>
+                <CommentsProvider>
                     <App />
-                </CommentsContext>
-            </PostsContext>
-        </UsersContext>
+                </CommentsProvider>
+            </PostsProvider>
+        </UsersProvider>
     </BrowserRouter>
 );
